ca4ab38692057f8ca11d64a07bef475ff2f7a442
Witzcode0 <brijesh.gondaliya07@gmail.com>
2026-01-29 14:44:00 +0530

    GQL part 1

---
+++ GQL/server.js
@@ -0,0 +1,34 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+// Schema:
+const typeDefs = gql`
+type Query {
+    greet: String
+}
+`;
+
+// Query Resolver:
+const resolvers = {
+    Query: {
+        greet: () => "Hello, GraphQL",
+    },
+};
+
+// Apollo Server Setup:
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], // The ApolloServerPluginLandingPageGraphQLPlayground enables a web-based GraphQL IDE to interact with the server.
+});
+
+// Start the Server:
+// Specify Custom Port: 
+
+const PORT = 5000; // Change to your desired port
+
+
+server.listen({ port: PORT }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
+
